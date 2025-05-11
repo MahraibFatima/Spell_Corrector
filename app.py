76b75e233aa8b8556605b1dcad4143b4877a1dbb
MahraibFatima <mahraibfatima5@gmail.com>
2025-05-11 17:02:00 +0500

    streamlit functions

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("hello")
+import streamlit as st
+
+st.title('Spell Checker')
+
+text = st.text_area("Enter Text:", value='', height=None, max_chars=None, key=None)
+
+if st.button('Check Spelling'):
+    st.balloons()
